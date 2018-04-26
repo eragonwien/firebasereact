4e609bd295ece00dc11d50c5187f08f16508e8dc
eragonwien <nguyennhoangson@gmail.com>
2018-04-26 15:39:42 +0200

    add basic components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+            <Header />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/Login' component={Login} />
+              <Route exact path='/Signup' component={Signup} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to React Router Tutorial</h2>
+        <ul>
+          <li><Link to={'/'}>Home</Link></li>
+          <li><Link to={'/Login'}>Login</Link></li>
+          <li><Link to={'/Signup'}>Signup</Link></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Header;
