ea188644ec8f6881210a33398d5cd9771a524382
SonLaptop\erago <eragonwien@gmail.com>
2018-04-26 18:08:47 +0200

    add login form

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,12 +1,57 @@
 import React, { Component } from 'react';
+import { Container, Form, Button, Segment } from 'semantic-ui-react';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: null,
+      password: null,
+      remember: false
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  
+
   render() {
     return (
-      <div>
-        <h1>Login</h1>
-      </div>
+      <Container >
+        <Segment inverted color='brown'>
+          <Form onSubmit={this.onSubmit} inverted>
+            <Form.Group widths='equal'>
+              <Form.Input
+                name='email'
+                value={this.state.email}
+                placeholder='Email'
+                onChange={this.onChange} />
+              <Form.Input
+                name='password'
+                value={this.state.password}
+                placeholder='Password'
+                onChange={this.onChange} />
+            </Form.Group>
+            <Form.Checkbox label='remember me' />
+            <Button type='submit'>Log In</Button>
+          </Form>
+        </Segment>
+      </Container>
     );
+  }
+
+  onChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  onSubmit() {
+    alert(this.state.email + ' ' + this.state.password + ' ' + this.state.remember.toString());
+    this.setState({
+      email: null,
+      password: null
+    });
   }
 }
 
